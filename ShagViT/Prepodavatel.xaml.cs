d03f7a715bdd2301ad49c29d2d5ae14a7fef9363
admin_2 <admin_2@PC3205>
2024-01-24 12:06:43 +0300

    Добавьте файлы проекта.

---
+++ ShagViT/Prepodavatel.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShagViT
+{
+    /// <summary>
+    /// Логика взаимодействия для Prepodavatel.xaml
+    /// </summary>
+    public partial class Prepodavatel : Window
+    {
+        public Prepodavatel()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ShagViT/Prepodavatel2.0.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShagViT
+{
+    /// <summary>
+    /// Логика взаимодействия для Prepodavatel2.xaml
+    /// </summary>
+    public partial class Prepodavatel2 : Window
+    {
+        public Prepodavatel2()
+        {
+            InitializeComponent();
+        }
+    }
+}
